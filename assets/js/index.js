8eaa548c06534e5de51900f7f3261719129cc541
Juan Agustín Zitelli <juanzitelli7@gmail.com>
2021-06-19 19:49:09 -0300

    Constructed boardData dynamically

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,66 @@
-console.log("Howdy!")
+const KNOWN_CSS_CLASSES = {
+  board: "game-area__board",
+};
+
+const GAME_CONFIG = {
+  board: {
+    dimension: 8,
+    checkersRows: 3,
+  },
+  players: {
+    p1: {
+      checkerIdentifier: 1,
+    },
+    p2: {
+      checkerIdentifier: 2,
+    },
+  },
+};
+
+const bootstrapApp = ({ players, board: { dimension, checkersRows } }) => {
+  const [board] = document.getElementsByClassName(KNOWN_CSS_CLASSES.board);
+
+  const getCheckerIdentifier = (rowId, cellId) => {
+    // TODO: Abstract this function and make it less verbose.
+    if (player1Rows.includes(rowId)) {
+      if (rowId % 2 !== 0) {
+        if (cellId % 2 === 0) {
+          return players.p1.checkerIdentifier;
+        }
+      } else {
+        if (cellId % 2 !== 0) {
+          return players.p1.checkerIdentifier;
+        }
+      }
+    } else if (player2Rows.includes(rowId)) {
+      if (rowId % 2 !== 0) {
+        if (cellId % 2 === 0) {
+          return players.p2.checkerIdentifier;
+        }
+      } else {
+        if (cellId % 2 !== 0) {
+          return players.p2.checkerIdentifier;
+        }
+      }
+    }
+  };
+
+  // TODO: Merge these two functions into a single one.
+  const player1Rows = [...Array(dimension).keys()].filter(
+    (_, index) => index < checkersRows
+  );
+
+  const player2Rows = [...Array(dimension).keys()].filter(
+    (_, index) => index >= dimension - checkersRows
+  );
+
+  const boardData = Array(dimension)
+    .fill(null)
+    .map((_, rowId) =>
+      Array(dimension)
+        .fill(null)
+        .map((_, cellId) => getCheckerIdentifier(rowId, cellId) ?? null)
+    );
+};
+
+bootstrapApp(GAME_CONFIG);

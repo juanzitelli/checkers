629432e8c43dbbf5cac8c91001c975ce93cddba2
Juan Agustín Zitelli <juanzitelli7@gmail.com>
2021-06-21 21:34:01 -0300

    Feature: JavaScript app bootstrapping (#4)
    * Added and linked JS index file
    
    * Added tile and checker templates
    
    * Removed board content and has-x-piece css classes
    
    * Added scores area
    
    * Constructed boardData dynamically
    
    * Added row template and replaced svg with img
    
    * Renamed tile template
    
    * Fixed image naming bug
    
    * Removed height and width from white checker image and added a descriptive class.
    
    * Added breakpoints-and-tile-related variables
    
    * Replaced flex display for grid in board row
    
    * Made board tiles responsive using the `clamp` function
    
    * Fixed tile class naming
    
    * Added `aspect-ratio` to images and defined their size
    
    * Added common css classes related to items inside HTML templates
    
    * Added a config object with template IDs
    
    * Created a function to access a template's content
    
    * Added a function to append rows to the checkers board
    
    * Added a function to get the initial board matrix
    
    * Added a function to render the game board
    
    * Aligned button and board to the center of the screen
    
    * Removed unnecessary selector chaining

---
+++ assets/js/index.js
@@ -0,0 +1,155 @@
+const KNOWN_CSS_CLASSES = {
+  board: "game-area__board",
+  row: "board__row",
+  tile: "row__tile",
+  whiteChecker: "checker--white",
+  redChecker: "checker--red",
+};
+
+const KNOWN_HTML_TEMPLATE_IDS = {
+  board: {
+    row: "row",
+    tile: "tile",
+    checkers: {
+      red: "red-piece",
+      white: "white-piece",
+    },
+  },
+};
+
+const GAME_CONFIG = {
+  board: {
+    dimension: 8,
+    checkersRows: 3,
+  },
+  players: {
+    p1: {
+      checkerIdentifier: 1,
+    },
+    p2: {
+      checkerIdentifier: 2,
+    },
+  },
+};
+
+const getContentFromTemplate = ({ templateId, elementCssClass }) => {
+  const template = document.getElementById(templateId).content.cloneNode(true);
+  return document
+    .importNode(template, true)
+    .querySelector(`.${elementCssClass}`);
+};
+
+const renderRows = (boardElement, initialBoardMatrix) => {
+  const rowElementParams = {
+    templateId: KNOWN_HTML_TEMPLATE_IDS.board.row,
+    elementCssClass: KNOWN_CSS_CLASSES.row,
+  };
+
+  const rowElement = getContentFromTemplate(rowElementParams);
+
+  const tileElementParams = {
+    templateId: KNOWN_HTML_TEMPLATE_IDS.board.tile,
+    elementCssClass: KNOWN_CSS_CLASSES.tile,
+  };
+
+  const tileElement = getContentFromTemplate(tileElementParams);
+
+  const whiteCheckerParams = {
+    templateId: KNOWN_HTML_TEMPLATE_IDS.board.checkers.white,
+    elementCssClass: KNOWN_CSS_CLASSES.whiteChecker,
+  };
+
+  const whiteCheckerElement = getContentFromTemplate(whiteCheckerParams);
+
+  const redCheckerParams = {
+    templateId: KNOWN_HTML_TEMPLATE_IDS.board.checkers.red,
+    elementCssClass: KNOWN_CSS_CLASSES.redChecker,
+  };
+
+  const redCheckerElement = getContentFromTemplate(redCheckerParams);
+
+  initialBoardMatrix.forEach((row) => {
+    const clonedRow = rowElement.cloneNode(true);
+    row.forEach((cell) => {
+      const clonedTile = tileElement.cloneNode(true);
+      switch (cell) {
+        case 1:
+          const clonedWhiteChecker = whiteCheckerElement.cloneNode(true);
+          clonedTile.appendChild(clonedWhiteChecker);
+          clonedRow.appendChild(clonedTile);
+          break;
+        case 2:
+          const clonedRedChecker = redCheckerElement.cloneNode(true);
+          clonedTile.appendChild(clonedRedChecker);
+          clonedRow.appendChild(clonedTile);
+          break;
+
+        default:
+          clonedRow.appendChild(clonedTile);
+          break;
+      }
+    });
+    boardElement.appendChild(clonedRow);
+  });
+};
+
+const getInitialBoardMatrix = ({ dimension, checkersRows, players }) => {
+  // TODO: Merge these two functions into a single one.
+  const player1Rows = [...Array(dimension).keys()].filter(
+    (_, index) => index < checkersRows
+  );
+
+  const player2Rows = [...Array(dimension).keys()].filter(
+    (_, index) => index >= dimension - checkersRows
+  );
+
+  const getCheckerIdentifier = (rowId, cellId) => {
+    // TODO: Abstract this function and make it less verbose.
+    if (player1Rows.includes(rowId)) {
+      if (rowId % 2 !== 0) {
+        if (cellId % 2 === 0) {
+          return players.p1.checkerIdentifier;
+        }
+      } else {
+        if (cellId % 2 !== 0) {
+          return players.p1.checkerIdentifier;
+        }
+      }
+    } else if (player2Rows.includes(rowId)) {
+      if (rowId % 2 !== 0) {
+        if (cellId % 2 === 0) {
+          return players.p2.checkerIdentifier;
+        }
+      } else {
+        if (cellId % 2 !== 0) {
+          return players.p2.checkerIdentifier;
+        }
+      }
+    }
+  };
+
+  return Array(dimension)
+    .fill(null)
+    .map((_, rowId) =>
+      Array(dimension)
+        .fill(null)
+        .map((_, cellId) => getCheckerIdentifier(rowId, cellId) ?? null)
+    );
+};
+
+const bootstrapApp = ({ players, board: { dimension, checkersRows } }) => {
+  const [boardElement] = document.getElementsByClassName(
+    KNOWN_CSS_CLASSES.board
+  );
+
+  const matrixGenerationParams = {
+    dimension,
+    checkersRows,
+    players,
+  };
+
+  const initialBoardMatrix = getInitialBoardMatrix(matrixGenerationParams);
+  renderRows(boardElement, initialBoardMatrix);
+};
+
+window.onload = bootstrapApp(GAME_CONFIG);

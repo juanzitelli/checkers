715f12a82031dc9de1305f7a72fb404732586958
Juan Agustín Zitelli <juanzitelli7@gmail.com>
2021-07-18 01:54:24 -0300

    Created a function to post form data as a JSON string and return the response

--- assets/js/form.js
+++ assets/js/form.js
@@ -9,3 +9,26 @@ const KNOWN_EVENT_NAMES = {
 const KNOWN_HTTP_METHODS = {
   post: "post",
 };
+
+async function postFormDataAsJson({ url, formData }) {
+  const plainFormData = Object.fromEntries(formData.entries());
+  const formDataJsonString = JSON.stringify(plainFormData);
+
+  const fetchOptions = {
+    method: KNOWN_HTTP_METHODS.post,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: formDataJsonString,
+  };
+
+  const response = await fetch(url, fetchOptions);
+
+  if (!response.ok) {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
+
+  return response.json();
+}
